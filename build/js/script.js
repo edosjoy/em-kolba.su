520ebbdc3c96c97bab5d832614fe5000bca6fb03
edosjoy <edosjoy@yandex.ru>
2025-12-11 23:03:30 +0300

    Add categories on a tile. Change any css styles

--- build/js/script.js
+++ build/js/script.js
@@ -1,2 +1,2 @@
-console.log("js file");
+function e(){return[{image:{src:"./images/tiles/myasodelie.jpeg",alt:"Сайт myasodelie.ru"},heading:"Мясоделие",text:"Товары для производства колбас",link:"https://myasodelie.ru/",categories:["Рецепты","Все для приготовления"]}]}console.log("js file");
 //# sourceMappingURL=script.js.map
