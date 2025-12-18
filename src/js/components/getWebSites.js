3dc701c61b7fb4844c618b811523e09b0d6adfa9
edosjoy <edosjoy@yandex.ru>
2025-12-18 20:42:10 +0300

    Refactor

--- src/js/components/getWebSites.js
+++ src/js/components/getWebSites.js
@@ -2,44 +2,279 @@ function getWebSites() {
     return [
         {
             image: {
-                src: './images/tiles/myasodelie.jpeg',
-                alt: 'Сайт myasodelie.ru',
+                src: './images/tiles/rezept-by.jpeg',
+                alt: 'Сайт rezept-by.ru',
             },
-            heading: 'Мясоделие-1',
-            text: 'Товары для производства колбас',
-            link: 'https://myasodelie.ru/',
+            heading: 'Рецепты бульбаша',
+            text: 'Современное предприятие, специализирующееся на производстве высококачественных колбас и мясных деликатесов',
+            link: 'https://rezept-by.ru/',
             categories: [
-                'Рецепты',
-                'Все для приготовления',
+                'Производители колбас',
+                'Колбаса оптом',
             ]
         },
 
         {
             image: {
-                src: './images/tiles/myasodelie.jpeg',
-                alt: 'Сайт myasodelie.ru',
+                src: './images/tiles/gostpp_ru.jpeg',
+                alt: 'Сайт www.gostpp.ru',
             },
-            heading: 'Мясоделие-2',
-            text: 'Товары для производства колбас',
-            link: 'https://myasodelie.ru/',
+            heading: 'ИП Ховренок',
+            text: 'Компания "ИП Ховренок" является одной из головных компаний на территории РФ занимающейся реализацией продукции производимой в Республике Беларусь',
+            link: 'https://www.gostpp.ru/',
+            categories: [
+                'Производители колбас',
+                'Колбаса оптом',
+                'Магазины колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/red-znamya_ru.jpg',
+                alt: 'Сайт red-znamya.ru',
+            },
+            heading: 'Красное знамя',
+            text: '"Красное знамя" - современная экосистема производственных предприятий, расположенных в городе Великие Луки, Псковской области',
+            link: 'https://red-znamya.ru/catalog/myasnaya-produktsiya/kolbasy/',
+            categories: [
+                'Производители колбас',
+                'Магазины колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/hlebopechka_ru.jpg',
+                alt: 'Сайт hlebopechka.ru',
+            },
+            heading: 'Рецепты колбас',
+            text: 'Большое количество различных рецептов домашней колбасы',
+            link: 'https://hlebopechka.ru/a/index.php?board=594.0',
+            categories: [
+                'Рецепты колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/kartaslov-ru.jpg',
+                alt: 'Сайт kartaslov.ru',
+            },
+            heading: 'Как делать колбасы',
+            text: 'Как делать колбасы в домашних условиях',
+            link: 'https://kartaslov.ru/книги/Как_делать_колбасы_в_домашних_условиях',
+            categories: [
+                'Рецепты колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/elhovkampk-ru.jpg',
+                alt: 'Сайт elhovkampk.ru',
+            },
+            heading: 'Елховский мясокомбинат',
+            text: 'Елховский мясокомбинат занимается изготовлением мясных изделий и деликатесов высокого качества',
+            link: 'https://elhovkampk.ru/-catalog',
+            categories: [
+                'Производители колбас',
+                'Колбаса оптом',
+                'Магазины колбас'
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/zdoroveevo-ru.jpg',
+                alt: 'Сайт zdoroveevo.ru',
+            },
+            heading: 'Здоровеево',
+            text: 'Интернет-магазин для здорового образа жизни и здорового питания',
+            link: 'https://www.zdoroveevo.ru/catalog/tovary-dlya-proizvodstva-kolbasok/',
+            categories: [
+                'Товары для производства колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/kazandelikates-tatar.jpg',
+                alt: 'Сайт kazandelikates.tatar',
+            },
+            heading: 'Казанские Деликатесы',
+            text: 'Молодая и динамично развивающаяся компания, которая была зарегистрирована в 2022 году и начала выпуск своей продукции в 2023 году',
+            link: 'https://kazandelikates.tatar/',
+            categories: [
+                'Производители колбас',
+                'Колбаса оптом'
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/shop-kolbasa-ru.jpg',
+                alt: 'Сайт shop.kolbasa.ru',
+            },
+            heading: 'Мясницкий ряд',
+            text: 'Высокотехнологичное предприятие, оснащенное современным оборудованием от ведущих мировых производителей, сертифицированное по международной системе',
+            link: 'https://shop.kolbasa.ru/catalog/kolbasy/',
             categories: [
                 'Магазины колбас',
-                'Все для приготовления',
             ]
         },
 
         {
             image: {
-                src: './images/tiles/myasodelie.jpeg',
-                alt: 'Сайт myasodelie.ru',
+                src: './images/tiles/vk-com-bsculinar.jpg',
+                alt: 'Сайт vk.com/bsculinar',
             },
-            heading: 'Мясоделие-3',
-            text: 'Товары для производства колбас',
-            link: 'https://myasodelie.ru/',
+            heading: 'Домашняя колбаса',
+            text: 'Домашняя колбаса: самые вкусные рецепты',
+            link: 'https://vk.com/bsculinar',
             categories: [
-                'Рецепты',
-                'Видео',
+                'Рецепты колбас',
             ]
-        }
+        },
+
+        {
+            image: {
+                src: './images/tiles/bsculinar-ru.jpg',
+                alt: 'Сайт bsculinar.ru',
+            },
+            heading: 'Биостар-кулинар',
+            text: 'Это пищевые ингредиенты, колбасные оболочки, оборудование для мясопереработки, а так же обучение и технологическое сопровождение',
+            link: 'https://bsculinar.ru/',
+            categories: [
+                'Товары для производства колбас',
+                'Обучение производству колбас',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/rutube-ru-tags-video-455787.jpg',
+                alt: 'Сайт rutube.ru',
+            },
+            heading: 'Видео рецепты',
+            text: 'Как сделать домашнюю колбасу',
+            link: 'https://rutube.ru/tags/video/455787/',
+            categories: [
+                'Рецепты колбас',
+                'Видео'
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
+
+        {
+            image: {
+                src: './images/tiles/',
+                alt: 'Сайт ',
+            },
+            heading: '',
+            text: '',
+            link: '',
+            categories: [
+                '',
+                '',
+            ]
+        },
     ];
 }
